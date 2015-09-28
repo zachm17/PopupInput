dba93596fc876bb5a6ff23f8b494e55eea6b52c1
zachm17 <zachmoy17@gmail.com>
2015-09-28 09:33:17 -0600

    Added comments to grab.answer

--- src/input/view/PopupDisplay.java
+++ src/input/view/PopupDisplay.java
@@ -8,10 +8,21 @@ import javax.swing.JOptionPane;
  */
 public class PopupDisplay
 {
+	/**
+	 * Displays the supplied text as a popup window.
+	 * @param wordsFromSomewhere
+	 */
 	public void showResponse(String wordsFromSomewhere)
 	{
 		JOptionPane.showMessageDialog(null, wordsFromSomewhere);
 	}
+	
+	/**
+	 * Displays a popup with the supplied String parameter with a field to type in a response.
+	 * It returns the answer to the supplied question as a String.
+	 * @param stuff A question to bes displayed in the popup window.
+	 * @return The user's input as a String data type.
+	 */
 	public String grabAnswer(String stuff)
 	{
 		String answer = "";
